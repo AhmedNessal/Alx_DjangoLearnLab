7ceb6c40caf9d2a93e48425644306c69c1b485fb
AhmedNessal <nessalahmed11@gmail.com>
2026-02-07 20:56:51 +0200

    week 12 task 2

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from api_project.api.views import BookList
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('books/', BookList.as_view(), name='book-list'),  
+    path('books/', BookList.as_view(), name='book-list'),
+    path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,3 +6,7 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet): # type: ignore
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
