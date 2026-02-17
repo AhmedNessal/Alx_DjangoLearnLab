4d79244912fc41022434a57a59242f5e6cd99063
AhmedNessal <nessalahmed11@gmail.com>
2026-02-17 21:58:38 +0200

    udpate urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,18 +1,20 @@
-from django.urls import path
-from . import views
+from django import views
 from .views import (
     PostListView, PostDetailView,
     PostCreateView, PostUpdateView, PostDeleteView
 )
+from django.urls import path
 
 urlpatterns = [
-    path("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
-    path("profile/", views.profile_view, name="profile"),
     path('posts/', PostListView.as_view(), name='post-list'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    path("register/", views.register_view, name="register"), # type: ignore
+    path("login/", views.login_view, name="login"), # type: ignore
+    path("logout/", views.logout_view, name="logout"), # type: ignore
+    path("profile/", views.profile_view, name="profile"),   # type: ignore
 ]
