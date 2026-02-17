ea53ffbcae4a8664aadaa968239330f951af0119
AhmedNessal <nessalahmed11@gmail.com>
2026-02-17 18:42:20 +0200

    update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,55 +1,35 @@
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 class BookListView(generics.ListAPIView):
-    """
-    ListView:
-    - Allows anyone to retrieve all books.
-    - Read-only access for unauthenticated users.
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class BookDetailView(generics.RetrieveAPIView):
-    """
-    DetailView:
-    - Allows anyone to retrieve a single book by ID.
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class BookCreateView(generics.CreateAPIView):
-    """
-    CreateView:
-    - Allows authenticated users to add new books.
-    - Uses serializer validation automatically.
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
 class BookUpdateView(generics.UpdateAPIView):
-    """
-    UpdateView:
-    - Allows authenticated users to update an existing book.
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
 class BookDeleteView(generics.DestroyAPIView):
-    """
-    DeleteView:
-    - Allows authenticated users to delete a book.
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+
