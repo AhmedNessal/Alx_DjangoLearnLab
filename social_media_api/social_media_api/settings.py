9100683327138a0850fd3065d536b39ef9b3cf2c
AhmedNessal <nessalahmed11@gmail.com>
2026-02-23 00:13:44 +0200

    task 1 week 15

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
-
+    'posts',
     'accounts',
 ]
 
@@ -127,7 +127,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
